c1ca34ef7c17159576c471dad0654dc18b250c5b
VyctorCosta <vyctorcosta00@gmail.com>
2022-04-21 15:15:38 -0300

    feat: App

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,50 +1,28 @@
 import React, { useState } from "react";
-import GlobalStyle from "../../GlobalStyles";
+import GlobalStyle from "../../styles/GlobalStyles";
 import { Container } from "./styles";
+import lightTheme from "../../styles/themes/light";
+import darkTheme from "../../styles/themes/dark";
 import Header from "../Header";
+import SearchBar from "../SearchBar";
+import Content from "../Content";
 import { ThemeProvider } from "styled-components";
-import {
-  General as lightGeneral,
-  Header as lightHeader,
-  SearchBar as lightSearchBar,
-  Content as lightContent,
-  Name as lightNome,
-} from "../../color/light";
-import {
-  General as darkGeneral,
-  Header as darkHeader,
-  SearchBar as darkSearchBar,
-  Content as darkContent,
-  Name as darkNome,
-} from "../../color/dark";
+import ThemeType from "../../types/themeType";
 
 function App() {
-  const lightTheme = {
-    General: lightGeneral,
-    Header: lightHeader,
-    SearchBar: lightSearchBar,
-    Content: lightContent,
-    Name: lightNome,
-  };
-  const darkTheme = {
-    General: darkGeneral,
-    Header: darkHeader,
-    SearchBar: darkSearchBar,
-    Content: darkContent,
-    Name: darkNome,
-  };
-  const [theme, setTheme] = useState(lightTheme);
+  const [theme, setTheme] = useState<ThemeType>(lightTheme);
 
   const toggleTheme = () => {
-    setTheme(theme.Name === "light" ? darkTheme : lightTheme);
+    setTheme(theme.name === "light" ? darkTheme : lightTheme);
   };
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Container>
-        <Header toggleTheme={toggleTheme} />
-        <h1>{theme.Name}</h1>
+        <Header theme={theme} toggleTheme={toggleTheme} />
+        <SearchBar />
+        <Content />
       </Container>
     </ThemeProvider>
   );
